a3b71b89010d9751fa9dde010de93999baff2bda
Shubham Pal <shubhampal31102003@gmail.com>
2024-07-25 23:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+// Logic to add details to the table
+const showPasswords = () => {
+    let tb = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    if (data == null) {
+        tb.innerHTML = "No Data To Show"
+    } else {
+        tb.innerHTML = `<tr>
+                <th>Website</th>
+                <th>Username</th>
+                <th>Password </th>
+                <th>Delete</th>
+            </tr>`
+        let arr = JSON.parse(data);
+        let str = ""
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            str += ` <tr>
+            <td>${element.web}</td>
+            <td>${element.user}</td>
+            <td>${element.pass}</td>
+            <td>${"Delete"}</td>
+            </tr > `
+        }
+        tb.innerHTML = tb.innerHTML + str
+    }
+}
 console.log("Working");
-document.querySelector(".btn").addEventListener("click",(e)=>{
+showPasswords()
+document.querySelector(".btn").addEventListener("click", (e) => {
     e.preventDefault()
+    console.log("Clicked...")
+    console.log(web.value, user.value, pass.value)
+    let passwords = localStorage.getItem("passwords")
+    console.log(passwords)
+    if (passwords == null) {
+        let json = []
+        json.push({ web: web.value, user: user.value, pass: pass.value })
+        alert("password saved");
+        localStorage.setItem("passwords", JSON.stringify(json))
+    }
+    else {
+        let json = JSON.parse(localStorage.getItem("passwords"))
+        json.push({ web: web.value, user: user.value, pass: pass.value })
+        alert("password saved");
+        localStorage.setItem("passwords", JSON.stringify(json))
+    }
+    showPasswords()
 })
