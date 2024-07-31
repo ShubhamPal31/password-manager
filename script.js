1ced0a5dd36e3858c95b35e62f65740aeec30645
Shubham Pal <shubhampal31102003@gmail.com>
2024-08-01 01:47:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// Logic to add details to the table
-
-const deletePassword=(web)=>{
-    let data=localStorage.getItem("passwords")
-    let arr = JSON.parse(data);
-    arrUpdated=arr.filter((e)=>{
-        return e.web!=web
-    })
-    localStorage.setItem("passwords",JSON.stringify(arrUpdated))
-    alert(`Successfully deleted ${web}'s password`)
-    showPasswords()
+function maskPassword(pass) {
+    let str = ""
+    for (let i = 0; i < pass.length; i++) {
+        str += "*";
+    }
+    return str;
 }
 
+function copy(txt) {
+    navigator.clipboard.writeText(txt).then(
+        () => {
+            console.log("Copied to clipboard successfully")
+            document.getElementById("span").style.display = "inline"
+            setTimeout(() => {
+                document.getElementById("span").style.display = "none"
+            }, 1500);
+        },
+        () => {
+            alert("Clipboard copying failed")
+        }
+    );
+}
 
+const deletePassword = (web) => {
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e) => {
+        return e.web != web
+    })
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated))
+    alert(`Successfully deleted ${web}'s password`)
+    showPasswords()
 
+}
+
+// Logic to add details to the table
 const showPasswords = () => {
     let tb = document.querySelector("table")
     let data = localStorage.getItem("passwords")
-    if (data == null) {
+    if (data == null || JSON.parse(data).length == 0) {
         tb.innerHTML = "No Data To Show"
     } else {
         tb.innerHTML = `<tr>
@@ -30,25 +51,25 @@ const showPasswords = () => {
         for (let i = 0; i < arr.length; i++) {
             const element = arr[i];
             str += ` <tr>
-            <td>${element.web} <img src="copy.svg" alt="Copy" width="12" height="12" onclick="copy(${element.web})">
+            <td>${element.web} <img class="icon" src="copy.svg" alt="Copy" width="12" height="12" onclick="copy('${element.web}')">
             </td>
-            <td>${element.user} <img src="copy.svg" alt="Copy" width="12" height="12" onclick="copy(${element.user})">
+            <td>${element.user} <img class="icon" src="copy.svg" alt="Copy" width="12" height="12" onclick="copy('${element.user}')">
             </td>
-            <td>${element.pass} <img src="copy.svg" alt="Copy" width="12" height="12" onclick="copy(${element.pass})">
+            <td>${maskPassword(element.pass)} <img class="icon" src="copy.svg" alt="Copy" width="12" height="12" onclick="copy('${element.pass}')">
             </td>
             <td><button class="btnsm" onclick="deletePassword('${element.web}')">Delete</button></td>
             </tr > `
         }
         tb.innerHTML = tb.innerHTML + str
     }
-    web.value=""
-    user.value=""
-    pass.value=""
+    web.value = ""
+    user.value = ""
+    pass.value = ""
 }
 console.log("Working");
 showPasswords()
 document.querySelector(".btn").addEventListener("click", (e) => {
-    e.preventDefault()                      
+    e.preventDefault()
     console.log("Clicked...")
     console.log(web.value, user.value, pass.value)
     let passwords = localStorage.getItem("passwords")
